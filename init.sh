dd416146fdabce214426502a0ba269330646ef2c
Nikita <nikita@shelimov.me>
2017-11-20 18:02:40 +0300

    added init file

---
+++ init.sh
@@ -0,0 +1,14 @@
+rm ~/.zshrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+rm ~/.eslintrc
+ln -s ~/dotfiles/.eslintrc ~/.eslintrc
+
+rm ~/.gitconfig
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
+rm ~/.vimrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+rm ~/.tern-config
+ln -s ~/dotfiles/.tern-config ~/.tern-config
