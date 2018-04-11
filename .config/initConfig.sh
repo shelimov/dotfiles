d38f032f73268c172ad022ebbdf24dbbb477e3d6
Nikita <nikita@shelimov.me>
2018-04-11 17:30:46 +0300

    added gtk style for terminal

--- .config/initConfig.sh
+++ .config/initConfig.sh
@@ -7,6 +7,9 @@ ln -s $DIR/fish ~/.config/fish
 rm -rf ~/.config/plank
 ln -s $DIR/plank ~/.config/plank
 
+rm -rf ~/.config/gtk-3.0
+ln -s $DIR/gtk-3.0 ~/.config/gtk-3.0
+
 #rm -rf ~/.config/fisherman ~/.config/fisherman
 #ln -s $DIR/fisherman ~/.config/fisherman
 
