4891890aae2a5201e4d248714bb7e8dfdbbc3448
Nikita <nikita@shelimov.me>
2018-04-11 16:13:00 +0300

    removed fisherman

--- .config/initConfig.sh
+++ .config/initConfig.sh
@@ -7,7 +7,7 @@ ln -s $DIR/fish ~/.config/fish
 rm -rf ~/.config/plank
 ln -s $DIR/plank ~/.config/plank
 
-rm -rf ~/.config/fisherman ~/.config/fisherman
-ln -s $DIR/fisherman ~/.config/fisherman
+#rm -rf ~/.config/fisherman ~/.config/fisherman
+#ln -s $DIR/fisherman ~/.config/fisherman
 
 $DIR/xfce4/initXfce.sh
