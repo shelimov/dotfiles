80ef4e8fba04f6970fa01611ea31ab382e37dbd0
Nikita <nikita@shelimov.me>
2018-12-19 18:09:55 +0300

    add message for gfgr

--- .config/fish/functions/gfgr.fish
+++ .config/fish/functions/gfgr.fish
@@ -1,4 +1,5 @@
 function gfgr -d "rebase current branch commits on top of remote branch"
+    echo 'Fetching branch origin and rebasing on top of it..'
     git fetch origin (__parse_git_branch)
     git rebase -m origin/(__parse_git_branch)
 end
