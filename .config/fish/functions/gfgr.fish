bca92c9d793cd1a8fdaa290d1fb6f2d4d3e3b1f1
Nikita <nikita@shelimov.me>
2018-12-19 18:02:46 +0300

    gfgr fix

--- .config/fish/functions/gfgr.fish
+++ .config/fish/functions/gfgr.fish
@@ -1,4 +1,4 @@
-function gfgr -d "javascript package manager"
+function gfgr -d "rebase current branch commits on top of remote branch"
     git fetch origin (__parse_git_branch)
-    git rebase -m origin/$(__parse_git_branch)
+    git rebase -m origin/(__parse_git_branch)
 end
