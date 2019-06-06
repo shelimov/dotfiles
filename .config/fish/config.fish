c77453b9277dd7551a906f5405d62455eb400ff4
Nikita <nikita@shelimov.me>
2019-06-06 17:12:58 +0300

    add lsa abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,4 @@ set pure_color_prompt_on_error (set_color --bold brred)
 set pure_symbol_git_dirty "✸"
 #set pure_color_git_dirty (set_color ff4d00)
 set pure_color_git_dirty (set_color ff7400)
+abbr lsa "ls -a"
