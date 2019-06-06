628c99bb57fe8a98ab710a3ce9d0a6a787745981
Nikita <nikita@shelimov.me>
2019-06-06 17:11:43 +0300

    apply new prompt colors and add gcm abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,6 @@
 set pure_symbol_prompt "~>"
+set pure_color_prompt_on_success (set_color --bold green)
+set pure_color_prompt_on_error (set_color --bold brred)
+set pure_symbol_git_dirty "✸"
+#set pure_color_git_dirty (set_color ff4d00)
+set pure_color_git_dirty (set_color ff7400)
