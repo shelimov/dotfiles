623a00cc688b84f7959a417b2c36f3e7db5e814b
Nikita <nikita@shelimov.me>
2019-09-30 17:42:57 +0300

    make gmo more explicit

--- .config/fish/functions/gmo.fish
+++ .config/fish/functions/gmo.fish
@@ -1,4 +1,4 @@
 function gmo -d "Merge origin/branch_name to local branch_name"
-    echo "git merge --ff origin/"(__parse_git_branch)
-    git merge --ff origin/(__parse_git_branch)
+    echo "git merge --ff "(__parse_git_branch)" origin/"(__parse_git_branch)
+    git merge --ff (__parse_git_branch) origin/(__parse_git_branch)
 end
