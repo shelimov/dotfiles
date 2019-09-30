691e7cf37da7be52e41e3b6878d65467b7b5f1b8
Nikita <nikita@shelimov.me>
2019-09-30 17:39:09 +0300

    fix gmo fn name

--- .config/fish/functions/gmo.fish
+++ .config/fish/functions/gmo.fish
@@ -1,4 +1,4 @@
-function gpu -d "Merge origin/branch_name to local branch_name"
+function gmo -d "Merge origin/branch_name to local branch_name"
     echo "git merge --ff origin/"(__parse_git_branch)
     git merge --ff origin/(__parse_git_branch)
 end
